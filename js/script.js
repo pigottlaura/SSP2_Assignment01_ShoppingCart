7d001e23dcafd23938467981766ddb5b0f600a7b
Laura Pigott <pigottlaura@gmail.com>
2016-10-26 20:38:42 +0100

    Adding Client side validation to forms

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
 window.onload = function(){
-    console.log("hi");
+    document.getElementById("login").addEventListener("submit", validateForm);
+    document.getElementById("register").addEventListener("submit", validateForm);
+}
+
+
+function validateForm(e){
+    var formValidated = true;
+    var formInputs = e.target.querySelectorAll("input:not([type='submit'])");
+
+    formInputs.forEach(function(input, i){
+
+        if(input.hasAttribute("required")) {
+            if(input.value.length == 0){
+                formValidated = false;
+                console.log("value required for " + input.getAttribute("name"));
+            }
+        }
+
+        if(input.hasAttribute("data-match")){
+            if(input.value != document.getElementsByName(input.getAttribute("data-match"))[0].value) {
+                formValidated = false;
+                console.log(input.getAttribute("name") + " does not match with " + input.getAttribute("data-match"));
+            }
+        }
+    });
+
+    if(formValidated){
+
+    } else {
+        e.preventDefault();
+    }
 }
