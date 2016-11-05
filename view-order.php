5aa33ac8765db554fba821e7bdbb63cad1095cf6
Laura Pigott <pigottlaura@gmail.com>
2016-11-05 11:23:38 +0000

    Ensuring only authorised users can view their own receipts

--- view-order.php
+++ view-order.php
@@ -1,17 +1,38 @@
 <?php
     include_once("./includes/actions/init.inc");
-    include("./../libs/mpdf/mpdf.php");
-    $mpdf = new mPDF();
-    $mpdf->SetDefaultFont("Aegean");
 
-    $receipt = Order::createReceipt($_GET["orderId"]);
-    $mpdf->writeHTML($receipt);
+    // Check that someone is logged in
+    if(isset($_SESSION["shopping_session"]->userId)) {
 
-    if(isset($_GET["action"]) && $_GET["action"] == "download"){
-        $filename = str_replace(" ", "", CONF_COMP_NAME) . "_Order#" . $_GET["orderId"] . "_Receipt.pdf";
-        $mpdf->output($filename, "D");
-        Functions::goToPage("page.php?page=view-my-orders");
+        // Check that they have requested a specific order
+        if(isset($_GET["orderId"])){
+
+            // Check that this order belongs to them
+            if(Database::getOrder($_GET["orderId"])){
+                include("./../libs/mpdf/mpdf.php");
+                $mpdf = new mPDF();
+                $mpdf->SetDefaultFont("Aegean");
+
+                $receipt = Order::createReceipt($_GET["orderId"]);
+                $mpdf->writeHTML($receipt);
+
+                if(isset($_GET["action"]) && $_GET["action"] == "download"){
+                    $filename = str_replace(" ", "", CONF_COMP_NAME) . "_Order#" . $_GET["orderId"] . "_Receipt.pdf";
+                    $mpdf->output($filename, "D");
+                    Functions::goToPage("page.php?page=view-my-orders");
+                } else {
+                    $mpdf->output();
+                }
+            } else {
+                // This order does not belong to this user
+                Functions::goToPage("page.php?page=view-my-orders");
+            }
+        } else {
+            // The user has not supplied an order id
+            Functions::goToPage("page.php?page=view-my-orders");
+        }
     } else {
-        $mpdf->output();
+        // This user is not logged in
+        Functions::goToPage("page.php?page=login-register");
     }
 ?>
